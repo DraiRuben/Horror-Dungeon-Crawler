2b20cea960d0dd72e726dabb6a346e0066ca1302
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2023-12-13 15:20:35 +0100

    AudioManager
    -add AudioManager
    -add AudioMixer
    -Resize Pause_Menu

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [Header("------------AudioSource------------")]
+
+    [SerializeField] AudioSource m_musicSource;
+    [SerializeField] AudioSource m_SFXSource;
+
+    [Header("------------AudioClip------------")]
+
+    public AudioClip background;
+
+    public void Start()
+    {
+        m_musicSource.clip = background;
+        m_musicSource.Play();
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        m_SFXSource.PlayOneShot(clip);
+    }
+}
--- Assets/Scripts/UI/Settings_Menu.cs
+++ Assets/Scripts/UI/Settings_Menu.cs
@@ -8,8 +8,10 @@ public class Settings_Menu : MonoBehaviour
 {
 
     [SerializeField] private AudioMixer m_mixer;
-    [SerializeField] private Slider m_slider;
-    
+    [SerializeField] private Slider m_masterVolumeSlider;
+    [SerializeField] private Slider m_musicVolumeSlider;
+    [SerializeField] private Slider m_SFXVolumeSlider;
+
     void Start()
     {
         if (PlayerPrefs.HasKey("masterVolume"))
@@ -19,6 +21,8 @@ public class Settings_Menu : MonoBehaviour
         else
         {
             SetMasterVolume();
+            SetMusicVolume();
+            SetSFXVolume();
         }
         gameObject.SetActive(false);
     }
@@ -26,14 +30,33 @@ public class Settings_Menu : MonoBehaviour
 
     public void SetMasterVolume()
     {
-        float volume = m_slider.value;
-        //m_mixer.SetFloat("master",Mathf.Log10(volume)*20);
+        float volume = m_masterVolumeSlider.value;
+        m_mixer.SetFloat("master",Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("masterVolume", volume);
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = m_musicVolumeSlider.value;
+        m_mixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = m_SFXVolumeSlider.value;
+        m_mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
     private void LoadVolume()
     {
-        m_slider.value = PlayerPrefs.GetFloat("masterVolume");
+        m_masterVolumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        m_musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        m_SFXVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
         SetMasterVolume();
+        SetMusicVolume();
+        SetSFXVolume();
     }
 }
