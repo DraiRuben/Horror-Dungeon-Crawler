68b7ca4e50acee196cd48d808b40d16b31f880c9
MisterHF <h.florianpro@gmail.com>
2023-12-15 12:29:27 +0100

    AI Boss1

--- Assets/Scripts/Enemies/Boss1AI.cs
+++ Assets/Scripts/Enemies/Boss1AI.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss2AI : MobAI
+public class Boss1AI : MobAI
 {
+    protected int Strength;
+    protected int AttackSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
